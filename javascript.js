f0e9b5fbf911df9e6fc70148861770c08f9b0aba
Josh Hancock <livingicon@gmail.com>
2022-02-05 11:37:21 -0500

    Moved playerSelection and computerSelection declaration to the proper place within the game() function so they would require multiple calls for each playRound call.

--- javascript.js
+++ javascript.js
@@ -2,17 +2,16 @@
 /*when not tied, push winner or loser to counters*/
 /*compare counter length and declare winner or loser*/
 
-const playerSelection = capitalize(prompt("Paper, Rock, or Scissors?"));
-const computerSelection = computerPlay();
-
-/*Currently only runs playRound*/
+/*Currently only runs the prompt once*/
 function game() {
-    return playRound(playerSelection, computerSelection);
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = capitalize(window.prompt("Paper, Rock, or Scissors?"));
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
 }
 
-console.log("player selection game: " + playerSelection);
-console.log("computer selection game: " + computerSelection);
-console.log("game result game: " + (game()));
+console.log(game());
 
 /*Helper Function 1: Computer randomly returns 'Rock', 'Paper' or 'Scissors'*/
 function computerPlay() {
@@ -20,7 +19,7 @@ function computerPlay() {
     return gladiators[Math.floor(Math.random() * gladiators.length)];
 }
 
-/*Helper Function 2: Capitalize function used to make playerSelection input uniform*/
+/*Helper Function 2: used in playerSelection function*/
 function capitalize(string) {
     let str2 = string
         .toLowerCase()
@@ -31,7 +30,7 @@ function capitalize(string) {
     return str1 + str2;
 }
 
-/*Helper Function 3: Plays round of game returning winner, loser or tie*/
+/*Helper Function 4: Plays round of game returning winner, loser or tie*/
 function playRound(playerSelection, computerSelection) {
     if (computerSelection === 'Rock' && playerSelection === 'Scissor') {
         return "Loser";
@@ -47,9 +46,7 @@ function playRound(playerSelection, computerSelection) {
         return "Winner";
     } else if (computerSelection === playerSelection) {
         return "Tie";
+    } else {
+        return "Invalid entry...check your spelling and try again."
     }
 }
-
-console.log("player selection PR: " + playerSelection);
-console.log("computer selection PR: " + computerSelection);
-console.log("game result PR: " + (playRound(playerSelection, computerSelection)));
