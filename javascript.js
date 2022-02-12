8597dc1404e7bba0a48cf53ef960c38aa90841d6
Josh Hancock <livingicon@gmail.com>
2022-02-12 11:34:13 -0500

    Add buttons in html and javascript without functionality

--- javascript.js
+++ javascript.js
@@ -1,18 +1,27 @@
+//Buttons
+btn-rock.addEventListener('click', function (e) {
+  console.log(e.target);
+});
+btn-paper.addEventListener('click', function (e) {
+  console.log(e.target);
+});
+btn-scissors.addEventListener('click', function (e) {
+  console.log(e.target);
+});
+  
 /*THE GAME (should declare a winner only after 5 valid games)*/
 function game() {
-    const winLossCounter = [];
-    for (let i = 0; winLossCounter.length < 5; i++) {
-        const playerSelection = capitalize(window.prompt("Paper, Rock, or Scissors?"));
-        const computerSelection = computerPlay();
-        /*This is where I got stuck. I was making the playerSelection and computerSelection outside of the for loop so they were only calling once*/
-        console.log(playRound(playerSelection, computerSelection));
-        if(playRound(playerSelection, computerSelection) !== "Tie" && playRound(playerSelection, computerSelection) !== "Invalid entry...check your spelling and try again.") {
-            winLossCounter.push(playRound(playerSelection, computerSelection));
-        }
-    } 
-    return winnerOrLoser(winLossCounter);
+  const winLossCounter = [];
+  for (let i = 0; winLossCounter.length < 5; i++) {
+    //const playerSelection = capitalize(window.prompt("Paper, Rock, or Scissors?"));
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+  if(playRound(playerSelection, computerSelection) !== "Tie" && playRound(playerSelection, computerSelection) !== "Invalid entry...check your spelling and try again.") {
+        winLossCounter.push(playRound(playerSelection, computerSelection));
+    }
+  } 
+  return winnerOrLoser(winLossCounter);
 }
-
 console.log(game());
 
 /*Helper Function 1: Computer randomly returns 'Rock', 'Paper' or 'Scissors'*/
@@ -21,7 +30,9 @@ function computerPlay() {
     return gladiators[Math.floor(Math.random() * gladiators.length)];
 }
 
+
 /*Helper Function 2: used in playerSelection function*/
+/*
 function capitalize(string) {
     let str2 = string
         .toLowerCase()
@@ -31,6 +42,7 @@ function capitalize(string) {
         .toUpperCase();
     return str1 + str2;
 }
+*/
 
 /*Helper Function 3: Plays round of game returning winner, loser or tie*/
 function playRound(playerSelection, computerSelection) {
@@ -48,9 +60,7 @@ function playRound(playerSelection, computerSelection) {
         return "Winner";
     } else if (computerSelection === playerSelection) {
         return "Tie";
-    } else {
-        return "Invalid entry...check your spelling and try again."
-    }
+    } 
 }
 
 /*Helper Function 4: Determines winner or loser*/
