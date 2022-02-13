fb9c7038a9f04392440295291f01600a0a1b9d8b
Josh Hancock <livingicon@gmail.com>
2022-02-13 01:14:25 -0500

    Convert rest of console.log to DOM methods and declare winner or loser

--- javascript.js
+++ javascript.js
@@ -1,71 +1,73 @@
 //DOM
-Rock.addEventListener('click', playRound);
-Paper.addEventListener('click', playRound);
-Scissors.addEventListener('click', playRound);
+ROCK.addEventListener('click', playRound);
+PAPER.addEventListener('click', playRound);
+SCISSORS.addEventListener('click', playRound);
 
 const rounds = document.querySelector('#rounds');
-//const playerWins = document.querySelector('#playerWins');
-//const computerWins = document.querySelector('#computerWins');
+const playerWins = document.querySelector('#playerWins');
+const computerWins = document.querySelector('#computerWins');
 
-//FUNCTIONS
-function computerPlay() {
-  const gladiators = ['Rock', 'Paper', 'Scissors'];
-  return gladiators[Math.floor(Math.random() * gladiators.length)];
-}
-
+//FUNCTION VARIABLES
 let counter = 0;
 let wins = 0;
 let losses = 0;
+computerWins.textContent = `${losses}`;
+playerWins.textContent = `${wins}`;
 
+//FUNCTIONS
 function playRound(e) {
   const playerSelection = e.target.id;
   const computerSelection = computerPlay();
-  if ((computerSelection === 'Rock' && playerSelection === 'Scissors') || 
-    (computerSelection === 'Paper' && playerSelection === 'Rock') || 
-    (computerSelection === 'Scissors' && playerSelection === 'Paper')) {
+  if ((computerSelection === 'ROCK' && playerSelection === 'SCISSORS') || 
+    (computerSelection === 'PAPER' && playerSelection === 'ROCK') || 
+    (computerSelection === 'SCISSORS' && playerSelection === 'PAPER')) {
+      counter++;
       const paragraph = document.createElement('p');
       paragraph.textContent = 
-      `Loser: the computer's ${computerSelection} 
-      beats your ${playerSelection}`;
+      `Round ${counter}: Loser
+      (Your ${playerSelection} 
+      loses to the computer's ${computerSelection}.)`;
       rounds.appendChild(paragraph);
       losses++;
-      console.log("losses: " + losses);
-      counter++;
-      console.log("rounds: " + counter);
+      computerWins.textContent = `${losses}`;
       if (counter === 5) {
-        console.log("GAME OVER");
+        winOrLose(losses);
       }
-  } else if ((computerSelection === 'Rock' && playerSelection === 'Paper') || 
-    (computerSelection === 'Paper' && playerSelection === 'Scissors') || 
-    (computerSelection === 'Scissors' && playerSelection === "Rock")) {
+  } else if ((computerSelection === 'ROCK' && playerSelection === 'PAPER') || 
+    (computerSelection === 'PAPER' && playerSelection === 'SCISSORS') || 
+    (computerSelection === 'SCISSORS' && playerSelection === "ROCK")) {
+      counter++;
       const paragraph = document.createElement('p');
       paragraph.textContent = 
-      `Winner: your ${playerSelection} 
-      beats the computer's ${computerSelection}`;
+      `Round ${counter}: Winner
+      (Your ${playerSelection} 
+      beat the computer's ${computerSelection}.)`;
       rounds.appendChild(paragraph);
       wins++;
-      console.log("wins: " + wins);
-      counter++;
-      console.log("rounds: " + counter);
+      playerWins.textContent = `${wins}`;
       if (counter === 5) {
-        console.log("GAME OVER");
+        winOrLose(losses);
       }
   } else if (computerSelection === playerSelection) {
       const paragraph = document.createElement('p');
-      paragraph.textContent = `Tie: ${playerSelection} and ${computerSelection}`;
+      paragraph.textContent = `No Count (Tie): ${playerSelection} and ${computerSelection}`;
       rounds.appendChild(paragraph);
   }
 }
 
-/*
-function winnerOrLoser(winLossCounter) {
-    let winnerArray = winLossCounter.filter(winner => winner === "W");
-    if (winnerArray.length >= 3) {
-        console.log("You are the winner!");
-        return "You are the Winner!";
-    } else {
-        console.log("You lost...");
-        return "You lost...";
-    }
+function computerPlay() {
+  const gladiators = ['ROCK', 'PAPER', 'SCISSORS'];
+  return gladiators[Math.floor(Math.random() * gladiators.length)];
 }
-*/
+
+function winOrLose(losses) {
+  if (losses >= 3) {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = "YOU LOST. GAME OVER.";
+    rounds.appendChild(paragraph);
+  } else {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = "YOU WON. GAME OVER.";
+    rounds.appendChild(paragraph);
+  }
+}
