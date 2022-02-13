46a5838e4c949317274a626a435557d365c9554a
Josh Hancock <livingicon@gmail.com>
2022-02-12 23:58:04 -0500

    Add a counter to determine 5 rounds

--- javascript.js
+++ javascript.js
@@ -1,61 +1,64 @@
-//Buttons
+//DOM
 Rock.addEventListener('click', playRound);
 Paper.addEventListener('click', playRound);
 Scissors.addEventListener('click', playRound);
 
-//DOM methods
 const rounds = document.querySelector('#rounds');
+//const playerWins = document.querySelector('#playerWins');
+//const computerWins = document.querySelector('#computerWins');
 
+//FUNCTIONS
 function computerPlay() {
   const gladiators = ['Rock', 'Paper', 'Scissors'];
   return gladiators[Math.floor(Math.random() * gladiators.length)];
 }
-  
+
+let counter = 0;
+
 function playRound(e) {
   const playerSelection = e.target.id;
   const computerSelection = computerPlay();
   if ((computerSelection === 'Rock' && playerSelection === 'Scissors') || 
     (computerSelection === 'Paper' && playerSelection === 'Rock') || 
     (computerSelection === 'Scissors' && playerSelection === 'Paper')) {
-        const paragraph = document.createElement('p');
-        paragraph.textContent = `Loser: ${computerSelection} beats ${playerSelection}`;;
-        rounds.appendChild(paragraph);
+      const paragraph = document.createElement('p');
+      paragraph.textContent = 
+      `Loser: the computer's ${computerSelection} 
+      beats your ${playerSelection}`;
+      rounds.appendChild(paragraph);
+      counter++;
+      console.log(counter);
+      if (counter === 5) {
+        console.log("GAME OVER");
+      }
   } else if ((computerSelection === 'Rock' && playerSelection === 'Paper') || 
     (computerSelection === 'Paper' && playerSelection === 'Scissors') || 
     (computerSelection === 'Scissors' && playerSelection === "Rock")) {
       const paragraph = document.createElement('p');
-      paragraph.textContent = `Winner: ${playerSelection} beats ${computerSelection}`;
+      paragraph.textContent = 
+      `Winner: your ${playerSelection} 
+      beats the computer's ${computerSelection}`;
       rounds.appendChild(paragraph);
+      counter++;
+      console.log(counter);
+      if (counter === 5) {
+        console.log("GAME OVER");
+      }
   } else if (computerSelection === playerSelection) {
-    const paragraph = document.createElement('p');
-    paragraph.textContent = "Tie";
-    rounds.appendChild(paragraph);
-  } 
+      const paragraph = document.createElement('p');
+      paragraph.textContent = `Tie: ${playerSelection} and ${computerSelection}`;
+      rounds.appendChild(paragraph);
+  }
 }
-
-/*
-function game() {
-  const winLossCounter = [];
-  for (let i = 0; winLossCounter.length < 5; i++) {
-    const playerSelection = e.target.id;
-    const computerSelection = computerPlay();
-    console.log(playRound());
-  if(playRound() !== "Tie") {
-        winLossCounter.push(playRound());
-    }
-  } 
-  return winnerOrLoser(winLossCounter);
-}
-*/
 
 /*
 function winnerOrLoser(winLossCounter) {
-    let winnerArray = winLossCounter.filter(winner => winner === "Winner");
+    let winnerArray = winLossCounter.filter(winner => winner === "W");
     if (winnerArray.length >= 3) {
         console.log("You are the winner!");
         return "You are the Winner!";
     } else {
-        console.log("You lost..."");
+        console.log("You lost...");
         return "You lost...";
     }
 }
